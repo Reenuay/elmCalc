ab9cc46f3caaa8a9137134249a5a6b1ec6e8f054
Reenuay <reenuay777@gmail.com>
2020-04-18 19:48:16 +0400

    Add expression solving

--- src/Expression.elm
+++ src/Expression.elm
@@ -1,4 +1,12 @@
-module Expression exposing (Constant, Unary, Binary, Function, Expression)
+module Expression exposing
+  (
+    Constant(..),
+    Unary(..),
+    Binary(..),
+    Function(..),
+    Expression(..),
+    solve
+  )
 
 type Constant
   = PI
@@ -9,23 +17,14 @@ type Unary
   | Minus
 
 type Binary
-  = Add
-  | Sub
-  | Mul
-  | Div
-  | Mod
+  = Add | Sub
+  | Mul | Div
   | Pow
 
 type Function
-  = Abs
-  | Sign
-  | Round
-  | Sqrt
-  | Sin
-  | Cos
-  | Tan
-  | Log
-  | Log10
+  = Sin | Cos  | Tan
+  | Abs | Sgn  | Rnd
+  | Log | Sqrt | Log10
 
 type Expression
   = Number Float
@@ -33,3 +32,46 @@ type Expression
   | Unary Unary Expression
   | Binary Binary Expression Expression
   | Function Function Expression
+
+cFloat : Constant -> Float
+cFloat c =
+  case c of
+    PI -> pi
+    E -> e
+
+uFunc : Unary -> Float -> Float
+uFunc u =
+  case u of
+    Plus  -> identity
+    Minus -> negate
+
+bFunc : Binary -> Float -> Float -> Float
+bFunc b =
+  case b of
+    Add -> (+)
+    Sub -> (-)
+    Mul -> (*)
+    Div -> (/)
+    Pow -> (^)
+
+fFunc : Function -> Float -> Float
+fFunc f =
+  case f of
+    Sin   -> sin
+    Cos   -> cos
+    Tan   -> tan
+    Abs   -> abs
+    Sgn   -> \x -> if x == 0 then 0 else if x < 0 then -1 else 1
+    Rnd   -> round >> toFloat
+    Log   -> logBase e
+    Sqrt  -> sqrt
+    Log10 -> logBase 10
+
+solve : Expression -> Float
+solve x =
+  case x of
+    Number n     -> n
+    Constant c   -> cFloat c
+    Unary u y    -> uFunc u (solve y)
+    Binary b y z -> bFunc b (solve y) (solve z)
+    Function f y -> fFunc f (solve y)
