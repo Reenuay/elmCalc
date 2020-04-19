1ed8f1ef2efcef0b47c6363e8013e2c84386f58c
Reenuay <reenuay777@gmail.com>
2020-04-19 20:59:59 +0400

    Modify main function for tests

--- src/Main.elm
+++ src/Main.elm
@@ -1,33 +1,61 @@
-module Main exposing (Model, Msg, update, view, init)
+module Main exposing (Model, Msg, update, view, init, main)
 
-import Html exposing (..)
+import Html exposing (Html, div, input, text)
+import Html.Attributes exposing (value)
+import Html.Events exposing (onInput)
 import Browser
+import Parser
+import Expression
+import ExpressionParser
 
-type alias Model = {}
+type alias Model = {
+    content: String,
+    result: String
+  }
 
 init : Model
-init = {}
+init = {
+    content = "",
+    result = ""
+  }
 
-type Msg = Msg1 | Msg2
+type Msg = Change String
+
+handleInput : String -> String
+handleInput i =
+  let
+    toString : Parser.DeadEnd -> String
+    toString x = "Error at position: " ++ String.fromInt x.col
+  in
+  case ExpressionParser.run i of
+    Ok e ->
+      e
+      |> Expression.solve
+      |> String.fromFloat
+
+    Err dl ->
+      dl
+      |> List.map toString
+      |> List.foldl (\x y -> x ++ "\n" ++ y) ""
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Msg1 ->
-      model
-
-    Msg2 ->
-      model
+    Change newContent ->
+      { model | content = newContent, result = handleInput newContent }
 
 view : Model -> Html Msg
 view model =
   div []
-    [ text "New Sandbox" ]
+    [
+      input [ value model.content, onInput Change ] [],
+      div [] [ text model.result ]
+    ]
 
 main : Program () Model Msg
 main =
-    Browser.sandbox {
-        init = init,
-        view = view,
-        update = update
-    }
+  Browser.sandbox {
+    init = init,
+    view = view,
+    update = update
+  }
